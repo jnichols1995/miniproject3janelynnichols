8369146c0613478f8f5c336a782a0177d0613cd7
jnichols1995 <janelyn.nichols12@gmail.com>
2024-03-20 09:35:15 -0500

    Rearranged some code according to the tutorial in the db.py file and deleted an extra line of code from the __init__.py file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -29,13 +29,5 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    def create_app():
-        app = ...
-        # existing code omitted
-
-        from . import auth
-        app.register_blueprint(auth.bp)
-
-        return app
 
     return app
--- flaskr/db.py
+++ flaskr/db.py
@@ -4,22 +4,9 @@ import click
 from flask import current_app, g
 
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
 
 def init_db():
@@ -36,6 +23,19 @@ def init_db_command():
     click.echo('Initialized the database.')
 
 
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
